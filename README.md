f8419142db8cf90bfa8e1a11df15157f5a7c2e66
Catherine Felton <catherinefelton.design@gmail.com>
2023-04-17 23:18:52 -0400

    hangman game progress

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # hangman-project
 This is the first project for Javascript Fundamentals
 
-HANGMAN GAME
+# HANGMAN GAME
 
-Description:
+## Description:
 In each game, a player must figure out the mystery word within 6 guesses. 
 Each try, the player is prompted to enter a letter. If correct, the letter appears in the correct placement in the word. If wrong, a part of the hangman appears. The player coninues guessing letters until either all the letters in the word are placed, winning the game, or if the player runs out of tries. 
 
 
-Gameplay:
-To start the game, open your Terminal and run `node .` 
+## Gameplay:
+To start the game, open your Terminal and run `node .`
 To end the game, press `Ctrl + c`
+When prompted, the player should guess a letter. 
+If correct, the letter will replace the appropriate letter blank.
+If incorrect, the player will lose a turn and a part of the hangman will appear. 
+
+If the player makes a wrong guess 6 times, the full hangman will appear and they have lost the game. 
+Conversely, the player will win the game if they guess all the letters in the word. 
+
+
+
+### Author: Catherine Felton
 
 
--- index.js
+++ index.js
@@ -2,8 +2,129 @@ import prompt from "readline-sync";
 import wordBank from "./word-bank.js";
 
 
-console.log("Welcome to Hangman!\nPlease press Ctrl + c to stop");
+let guessesLeft = 6;
+let correctWord;
+let playerWord; 
+let playerGuessedLetter;
+let guessedLetters = [];
+let displayHangman;
 
-let letter = prompt.question("Please guess a letter: ");
-console.log(`You guessed ${letter}`);
+
+
+const getWord = () => {
+    let randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    correctWord = Array.from(randomWord);
+    displayWord(correctWord, playerWord);
+    
+};
+
+
+const displayWord = (correctWord, playerWord) => {
+    playerWord = [`_ `.repeat(correctWord.length)]
+    for (let i = 0; i < playerWord.length; i++) {
+        console.log(playerWord[i]);
+    } 
+    console.log(correctWord + "--answer")
+    isGameOver();
+    
+}; 
+
+
+
+
+const isAlreadyGuessed = (playerGuessedLetter, guessedLetters) => {
+    
+    if (guessedLetters.includes(playerGuessedLetter)) {
+        console.log("Letter already guessed")
+        isGameOver();
+
+       } else if (playerGuessedLetter.length > 1 || playerGuessedLetter.length < 1 ) {
+            console.log("Guess only one letter");
+            isGameOver();
+        } else { 
+            console.log("not guessed")
+                guessedLetters.push(playerGuessedLetter);
+                isGuessCorrect(playerGuessedLetter, correctWord);
+       }
+    }; 
+
+
+const isGuessCorrect = (playerGuessedLetter, correctWord) => {
+    console.log(guessedLetters + "--guessed letters");
+    if (correctWord.includes(playerGuessedLetter)) {
+        console.log("Correct")
+       //buildWord(playerGuessedLetter, playerWord, correctWord);
+    }  else {
+            console.log("Wrong")
+            triesRemaining();
+    };
+    
+};
+
+const buildWord = (playerGuessedLetter, playerWord, correctWord) => {
+   for (let j = 0; j < correctWord.length; j++) {
+    if(correctWord[j] === playerGuessedLetter) {
+        console.log(correctWord.indexOf(playerGuessedLetter) + "--index")
+    }
+    console.log(playerWord);
+   }
+} ;
+
+
+const triesRemaining = () => {   
+        guessesLeft--; 
+        console.log(`Guesses Remaining: ${guessesLeft}`);
+        addHangman(guessesLeft, displayHangman);
+};
+
+
+const addHangman = (guessesLeft, displayHangman) => {
+    let hangman = [`  \\`, `\n/`, `\n  |\n  |`, `  /`, `\n\\`, `  O`];
+    for (let i = 0; i < 1; i++) { 
+        displayHangman += hangman[guessesLeft];
+    }; 
+    console.log(displayHangman);  
+    isGameOver(guessesLeft);
+};
+    
+
+const isGameOver = (guessesLeft) => {
+    if (guessesLeft === 0)  {
+        console.log("You lost. Try again?");
+        guessLetter();
+        return false;
+
+    } else if (guessedLetters === correctWord) {
+       console.log(`You win! The answer was: ${correctWord}`);
+        return true;
+    } else {
+        guessLetter();
+        return true;        
+    }; 
+}; 
+
+const guessLetter = () => {
+    while (isGameOver) {  
+        playerGuessedLetter = prompt.question("\nPlease guess a letter: ");
+        playerGuessedLetter = playerGuessedLetter.toLowerCase();
+        console.log(`You guessed ${playerGuessedLetter}`);
+        isAlreadyGuessed(playerGuessedLetter, guessedLetters);         
+    };
+};
+
+
+const startGame = () => {
+    while (isGameOver) {
+        console.log("Welcome to Hangman!\nPlease press Ctrl + c to stop");
+        getWord();       
+    };  
+};
+
+startGame();
+
+
+
+ 
+
+
 
